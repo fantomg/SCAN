81c30aeb993fb921c4e7f16b93be73c6bbab0f56
fantomg <386015805@qq.com>
2023-10-30 21:44:49 +0800

    划分数据集

--- huafen.py
+++ huafen.py
@@ -1,14 +1,21 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# 读取 CSV 文件
-data = pd.read_csv("E:edge_user_buy_package.csv")
+# 读取CSV文件
+df = pd.read_csv("E:edge_user_buy_package.csv")  # 替换为您的CSV文件路径
 
-# 将数据帧划分为训练集和数据集
-train_data, test_data = train_test_split(data, test_size=0.004)
+# 划分训练集和测试集+验证集
+train_df, test_val_df = train_test_split(df, test_size=0.02, random_state=42)
 
-# 将训练集保存为新的CSV文件
-train_data.to_csv('train_data.csv', index=False)
+# 再次划分测试集和验证集
+test_df, val_df = train_test_split(test_val_df, test_size=0.5, random_state=42)
 
-# 将测试集保存为新的CSV文件
-test_data.to_csv('test_data.csv', index=False)
+# 打印训练集、测试集和验证集的行数
+print(f"训练集行数：{len(train_df)}")
+print(f"测试集行数：{len(test_df)}")
+print(f"验证集行数：{len(val_df)}")
+
+# 保存为新的CSV文件
+train_df.to_csv('train.csv', index=False)
+test_df.to_csv('test.csv', index=False)
+val_df.to_csv('val.csv', index=False)
--- tongji.py
+++
@@ -1,4 +0,0 @@
-# filename='E:edge_user_buy_package.txt'
-filename='E:edge_user_buy_package.txt'
-total = sum(1 for line in open(filename))
-print('The total lines is ',total)
